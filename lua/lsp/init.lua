91006a6718abb0796da23c4dce5e56b816ef07c5
Melker Österberg <melkerosterberg@gmail.com>
2020-12-19 21:42:48 +0100

    Rename 'nvim_lsp' to 'lspconfig'

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -3,8 +3,8 @@ local on_attach = function()
   require('diagnostic').on_attach()
 end
 
-require('nvim_lsp').tsserver.setup{on_attach=on_attach}
-require('nvim_lsp').pyls.setup{on_attach=on_attach}
-require('nvim_lsp').jdtls.setup{on_attach=on_attach}
-require('nvim_lsp').vimls.setup{on_attach=on_attach}
-require('nvim_lsp').sumneko_lua.setup{on_attach=on_attach}
+require('lspconfig').tsserver.setup{on_attach=on_attach}
+require('lspconfig').pyls.setup{on_attach=on_attach}
+require('lspconfig').jdtls.setup{on_attach=on_attach}
+require('lspconfig').vimls.setup{on_attach=on_attach}
+require('lspconfig').sumneko_lua.setup{on_attach=on_attach}
