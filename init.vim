c56df70beb2f8861d0b3851e934ef13c11c70ac3
Melker Österberg <melkerosterberg@gmail.com>
2021-09-29 09:59:34 +0200

    Configure kommentary to use context-commentstring

--- init.vim
+++ init.vim
@@ -522,7 +522,7 @@ call s:colorschemeMods()
 let g:indentLine_char = '▏'
 let g:indentLine_color_gui = '#4b5263'
 let g:indentLine_setConceal = 0 " Don't overwrite concealcursor and conceallevel
-let g:indentLine_fileTypeExclude = ['markdown', 'startify']
+let g:indentLine_fileTypeExclude = ['markdown', 'startify', 'sagahover']
 let g:indentLine_bufTypeExclude = ['fzf', 'help']
 let g:indent_blankline_buftype_exclude = ['help']
 let g:indent_blankline_show_first_indent_level = v:false
--- lua/config.lua
+++ lua/config.lua
@@ -363,7 +363,8 @@ require('nvim-treesitter.configs').setup {
     },
   },
   context_commentstring = {
-    enable = true
+    enable = true,
+    enable_autocmd = false,
   }
 }
 
@@ -459,6 +460,13 @@ local kommentary = require('kommentary.config')
 kommentary.setup()
 kommentary.configure_language('default', {
   prefer_single_line_comments = true
+})
+kommentary.configure_language('typescriptreact', {
+  single_line_comment_string = 'auto',
+  multi_line_comment_strings = 'auto',
+  hook_function = function()
+    require('ts_context_commentstring.internal').update_commentstring()
+  end,
 })
 
 map('n', 'cmm',  '<Plug>kommentary_line_default',   {noremap = false})
