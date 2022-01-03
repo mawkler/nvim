e174c9041eb7c83c73a7da77041b52285a904268
Melker Österberg <melkerosterberg@gmail.com>
2022-01-03 14:15:28 +0100

    Improve neoscroll animation and speed

--- lua/config.lua
+++ lua/config.lua
@@ -797,7 +797,21 @@ cmd 'highlight! link TSError Normal'
 ---------------
 -- Neoscroll --
 ---------------
-require('neoscroll').setup()
+require('neoscroll').setup {
+  easing_function = 'cubic',
+}
+
+local scroll_speed = 140
+
+require('neoscroll.config').set_mappings {
+  ['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', scroll_speed}},
+  ['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', scroll_speed}},
+  ['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', scroll_speed}},
+  ['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', scroll_speed}},
+  ['zt']    = {'zt', {scroll_speed}, 'sine'},
+  ['zz']    = {'zz', {scroll_speed}, 'sine'},
+  ['zb']    = {'zb', {scroll_speed}, 'sine'},
+}
 
 ----------------
 -- Diagnostics --
