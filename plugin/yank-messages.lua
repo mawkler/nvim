c3bcc0e6524148b296b3c4bb4519126059c750f2
Melker Österberg <melkerosterberg@gmail.com>
2022-03-09 09:24:28 +0100

    Add YankMessages command
    `:YankMessages` yanks the latest printed message into the clipboard (by default)
    
    To yank the 2 latest printed lines into the `"` register, do `2YankMessages "`

---
+++ plugin/yank-messages.lua
@@ -0,0 +1,19 @@
+local function getLatestMessages(count)
+  local messages = vim.fn.execute('messages')
+  local lines = vim.split(messages, '\n')
+  lines = vim.tbl_filter(function(line) return line ~= '' end, lines)
+  count = count and tonumber(count) or nil
+  count = (count ~= nil and count > 0) and count - 1 or #lines
+  return table.concat(vim.list_slice(lines, #lines - count), '\n')
+end
+
+local function yankMessages(register, count)
+  local default_register = '+'
+  register = (register and register ~= '') and register or default_register
+  vim.fn.setreg(register, getLatestMessages(count), 'l')
+end
+
+vim.api.nvim_add_user_command('YankMessages', function(opts)
+  return yankMessages(opts.reg, opts.count)
+end, { count = 10, register = true })
+
