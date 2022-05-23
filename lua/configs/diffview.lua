9759e78a96b93d6c8d7ef8d0d9cccc4c2a39e22a
Melker Österberg <melkerosterberg@gmail.com>
2022-05-23 16:19:44 +0200

    Update to new diffview config api

--- lua/configs/diffview.lua
+++ lua/configs/diffview.lua
@@ -5,58 +5,63 @@ return { 'sindrets/diffview.nvim',
   requires = 'nvim-lua/plenary.nvim',
   config = function()
     local map = require('../utils').map
+    local actions = require('diffview.config').actions
 
-    local dv_callback = require('diffview.config').diffview_callback
+    map('n', '<leader>gD', '<cmd>DiffviewOpen<CR>')
+    map('n', '<leader>gH', '<cmd>DiffviewFileHistory<CR>')
+
     require('diffview').setup {
       enhanced_diff_hl = false,
       file_panel = {
-        width = 40
+        win_config = {
+          width = 40
+        }
       },
       file_history_panel = {
-        height = 15,
+        win_config = {
+          height = 15,
+        }
       },
       key_bindings = {
         view = {
-          ['<C-j>'] = dv_callback('select_next_entry'),
-          ['<C-k>'] = dv_callback('select_prev_entry'),
-          ['<C-s>'] = dv_callback('goto_file_split'),
-          ['<C-t>'] = dv_callback('goto_file_tab'),
-          ['~']     = dv_callback('focus_files'),
-          ['`']     = dv_callback('toggle_files'),
+          ['<C-j>'] = actions.select_next_entry,
+          ['<C-k>'] = actions.select_prev_entry,
+          ['<C-s>'] = actions.goto_file_split,
+          ['<C-t>'] = actions.goto_file_tab,
+          ['~']     = actions.focus_files,
+          ['`']     = actions.toggle_files,
         },
         file_panel = {
-          ['<Space>']  = dv_callback('select_entry'),
-          ['<CR>']     = dv_callback('focus_entry'),
-          ['gf']       = dv_callback('goto_file_edit'),
-          ['<C-j>']    = dv_callback('select_next_entry'),
-          ['<C-k>']    = dv_callback('select_prev_entry'),
-          ['<C-t>']    = dv_callback('goto_file_tab'),
-          ['<Esc>']    = dv_callback('toggle_files'),
-          ['`']        = dv_callback('toggle_files'),
-          ['<space>e'] = dv_callback(),
-          ['<space>b'] = dv_callback()
+          ['<Space>']  = actions.select_entry,
+          ['<CR>']     = actions.focus_entry,
+          ['gf']       = actions.goto_file_edit,
+          ['<C-j>']    = actions.select_next_entry,
+          ['<C-k>']    = actions.select_prev_entry,
+          ['<C-t>']    = actions.goto_file_tab,
+          ['<Esc>']    = actions.toggle_files,
+          ['`']        = actions.toggle_files,
+          ['<space>e'] = false,
+          ['<space>b'] = false,
         },
         file_history_panel = {
-          ['!']        = dv_callback('options'),
-          ['<CR>']     = dv_callback('open_in_diffview'),
-          ['<Space>']  = dv_callback('select_entry'),
-          ['<C-j>']    = dv_callback('select_next_entry'),
-          ['<C-k>']    = dv_callback('select_prev_entry'),
-          ['gf']       = dv_callback('goto_file'),
-          ['<C-s>']    = dv_callback('goto_file_split'),
-          ['<C-t>']    = dv_callback('goto_file_tab'),
-          ['~']        = dv_callback('focus_files'),
-          ['`']        = dv_callback('toggle_files'),
-          ['<space>e'] = dv_callback(),
-          ['<space>b'] = dv_callback()
+          ['!']        = actions.options,
+          ['<CR>']     = actions.open_in_diffview,
+          ['<Space>']  = actions.select_entry,
+          ['<C-j>']    = actions.select_next_entry,
+          ['<C-k>']    = actions.select_prev_entry,
+          ['gf']       = actions.goto_file,
+          ['<C-s>']    = actions.goto_file_split,
+          ['<C-t>']    = actions.goto_file_tab,
+          ['~']        = actions.focus_files,
+          ['`']        = actions.toggle_files,
+          ['<space>e'] = false,
+          ['<space>b'] = false
         },
         option_panel = {
-          ['<CR>'] = dv_callback('select')
+          ['<CR>'] = actions.select_entry
         }
       }
     }
 
-    map('n', '<leader>gD', '<cmd>DiffviewOpen<CR>')
-    map('n', '<leader>gH', '<cmd>DiffviewFileHistory<CR>')
   end
 }
