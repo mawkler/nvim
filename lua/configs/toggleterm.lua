8ae5c503c10242528f14ffdcc279003a43a4ec0c
Melker Österberg <melkerosterberg@gmail.com>
2022-06-23 21:49:37 +0200

    Add gf/gF compatibility to ToggleTerm

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -3,8 +3,19 @@
 ----------------
 return { 'akinsho/toggleterm.nvim',
   keys = '<C-CR>',
+  cmd = {
+    'ToggleTerm',
+    'ToggleTermToggleAll',
+    'ToggleTermSendCurrentLine',
+    'ToggleTermSendVisualLines',
+    'ToggleTermSendVisualSelection',
+  },
   config = function()
-    require('toggleterm').setup {
+    local toggleterm, utils = require('toggleterm'), require('utils')
+    local map = utils.map
+    local api = vim.api
+
+    toggleterm.setup {
       open_mapping = '<C-CR>',
       direction = 'float',
       float_opts = {
@@ -16,5 +27,30 @@ return { 'akinsho/toggleterm.nvim',
         },
       },
     }
+
+    local function go_to_file(command)
+      local cursor = api.nvim_win_get_cursor(0)
+      local bufnr = api.nvim_get_current_buf()
+      toggleterm.toggle(0)
+      api.nvim_win_set_buf(0, bufnr)
+      api.nvim_win_set_cursor(0, cursor)
+      vim.cmd('norm! ' .. command)
+    end
+
+    api.nvim_create_augroup('ToggleTerm', {})
+    api.nvim_create_autocmd('TermOpen', {
+      pattern = 'term://*toggleterm#*',
+      callback = function()
+        map('n', 'gf', function() return go_to_file('gf') end, {
+          desc = 'Close toggleterm and go to file',
+          buffer = true,
+        })
+        map('n', 'gF', function() return go_to_file('gF') end, {
+          desc = 'Close toggleterm and go to file (and line number)',
+          buffer = true,
+        })
+      end,
+      group = 'ToggleTerm'
+    })
   end
 }
