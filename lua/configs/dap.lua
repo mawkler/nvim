fb0b14ac89407f8af61940baf0e56485e2b01e28
Melker Österberg <melkerosterberg@gmail.com>
2022-06-26 11:47:36 +0200

    Add jester (haven't gotten it to work yet though)

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -3,105 +3,50 @@
 ---------
 return { 'mfussenegger/nvim-dap',
   requires = {
-    'rcarriga/nvim-dap-ui',    -- UI for nvim-dap
-    'ravenxrz/DAPInstall.nvim' -- Installing/uninstalling debuggers
-                               -- temporary branch while dap-buddy is written
+    'rcarriga/nvim-dap-ui',     -- UI for nvim-dap
+    'ravenxrz/DAPInstall.nvim', -- Installing/uninstalling debuggers
+                                -- temporary branch while dap-buddy is written
+    'David-Kunz/jester',        -- Debugging Jest tests
   },
   keys = { '<F5>', '<F10>', '<F11>', '<F12>', '<F12>', '<F9>' },
   cmd = { 'DIInstall', 'DIUninstall', 'DIList' },
   config = function ()
-    -- local di = require('dap-install')
-    -- di.setup { verbosely_call_debuggers = true }
-    -- di.config('chrome', {})
-    -- di.config('jsnode')
-    -- di.config('python')
-    -- di.config('lua')
+    local dap, dap_ui, di = require('dap'), require('dapui'), require('dap-install')
+    local map = require('utils').map
 
-    -- require('dap.ext.vscode').load_launchjs()
-
-    local function map(modes, lhs, rhs, opts)
-      if (type(modes) ~= 'table') then modes = {modes} end
-
-      for _, mode in pairs(modes) do
-        local options = {noremap = true}
-        if opts then options = vim.tbl_extend('force', options, opts) end
-        vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-      end
-    end
-
-    -- local map = require('utils').map
-    -- map('n', '<F5>', require'dap'.continue)
+    -- DAPInstall --
+    di.config('jsnode')
 
     -- Mappings --
-    map('n', '<F5>', ':lua require("dap").continue() require("dapui").open()<CR>', {silent = true})
-    map('n', '<F10>', ':lua require"dap".step_over()<CR>', {silent = true})
-    map('n', '<F11>', ':lua require"dap".step_into()<CR>', {silent = true})
-    map('n', '<F12>', ':lua require"dap".step_out()<CR>', {silent = true})
-    map('n', '<F12>', ':lua require"dap".step_out()<CR>', {silent = true})
-    map('n', '<F9>',  ':lua require"dap".toggle_breakpoint()<CR>', {silent = true})
-    map('n', '<leader><F9>',  ':lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', {silent = true})
-    -- nnoremap <silent> <leader>dr :lua require"dap".repl.open()<CR>
-    -- nnoremap <silent> <leader>dl :lua require"dap".run_last()<CR>
+    map('n', '<F5>',  function()
+      dap.continue()
+      dap_ui.open()
+    end)
+    map('n', '<F10>', dap.step_over)
+    map('n', '<F11>', dap.step_into)
+    map('n', '<F12>', dap.step_out)
+    map('n', '<F12>', dap.step_out)
+    map('n', '<F9>',  dap.toggle_breakpoint)
+    map('n', '<leader><F9>',  function()
+      dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+    end)
+    map('n', '<leader>dr', dap.repl.open)
+    map('n', '<leader>dl', dap.run_last)
+    map('n', '<leader>de', dap_ui.eval)
 
-    -- -- dap-ui --
-    -- require('dapui').setup()
+    -- DAP-UI --
+    dap_ui.setup()
 
-    -- map('n', '<leader>De',  ':lua require("dapui").eval()<CR>', {silent = true})
+    -- Jester --
+    require('jester').setup({
+      path_to_jest_run = './node_modules/bin/jest'
+    })
 
-
-
-    -- local dap = require"dap"
-    -- dap.configurations.lua = {
-    --   {
-    --     type = 'nlua',
-    --     request = 'attach',
-    --     name = "Attach to running Neovim instance",
-    --     host = function()
-    --       local value = vim.fn.input('Host [127.0.0.1]: ')
-    --       if value ~= "" then
-    --         return value
-    --       end
-    --       return '127.0.0.1'
-    --     end,
-    --     port = function()
-    --       local val = tonumber(vim.fn.input('Port: '))
-    --       assert(val, "Please provide a port number")
-    --       return val
-    --     end,
-    --   }
-    -- }
-
-    -- dap.adapters.nlua = function(callback, config)
-    --   callback({ type = 'server', host = config.host, port = config.port })
-    -- end
-
-
-    local home = os.getenv('HOME')
-    local dap = require('dap')
     dap.adapters.node2 = {
       type = 'executable',
       command = 'node',
-      -- args = {home .. '/personal/microsoft-sucks/vscode-node-debug2/out/src/nodeDebug.js'},
-      args = { home .. '/.local/share/nvim/dapinstall/jsnode/vscode-node-debug2/out/src/nodeDebug.js' },
-    }
-
-    dap.configurations.javascript = {
-      {
-        name = 'Launch',
-        type = 'node2',
-        request = 'launch',
-        program = '${file}',
-        cwd = vim.loop.cwd(),
-        sourceMaps = true,
-        protocol = 'inspector',
-        console = 'integratedTerminal',
-      },
-      {
-        -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-        name = 'Attach to process',
-        type = 'node2',
-        request = 'attach',
-        processId = require('dap.utils').pick_process,
+      args = {
+        vim.fn.stdpath('data') .. '/dapinstall/jsnode/vscode-node-debug2/out/src/nodeDebug.js',
       },
     }
 
@@ -129,7 +74,20 @@ return { 'mfussenegger/nvim-dap',
         port = 9229,
         skipFiles = { '<node_internals>/**', 'node_modules/**' },
       },
+      {
+        name = 'op host start',
+        type = 'node2',
+        request = 'launch',
+        cwd = vim.loop.cwd(),
+        runtimeArgs = { '-r', 'ts-node/register' },
+        args = {'--inspect', '${file}'},
+        sourceMaps = true,
+        skipFiles = { '<node_internals>/**', 'node_modules/**' },
+      }
     }
+
+    -- Loads .vscode/launch.json files if available
+    require('dap.ext.vscode').load_launchjs(nil, { node = {'typescript'} })
+
   end,
 }
-
