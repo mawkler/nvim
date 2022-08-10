476823232553297dfb4bbcf576990bddfeaa63aa
Melker Österberg <melkerosterberg@gmail.com>
2022-08-10 08:16:35 +0200

    Use nvim_create_user_command for Http command

--- lua/configs/rest.lua
+++ lua/configs/rest.lua
@@ -8,7 +8,7 @@ return { 'NTBBloodbath/rest.nvim',
 
     require('rest-nvim').setup()
 
-    function _G.http_request()
+    local function http_request()
       if vim.api.nvim_win_get_width(vim.api.nvim_get_current_win()) < 80 then
         vim.cmd('wincmd s')
       else
@@ -17,14 +17,20 @@ return { 'NTBBloodbath/rest.nvim',
       vim.cmd('edit ~/.config/nvim/http | set filetype=http | set buftype=')
     end
 
-    vim.cmd 'command! Http call v:lua.http_request()'
+    vim.api.nvim_create_user_command('Http', http_request, {
+      desc = 'Send HTTP request',
+    })
 
     vim.api.nvim_create_augroup('RestNvim', {})
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'http',
       callback = function()
-        map('n', '<CR>', '<Plug>RestNvim:w<CR>', { buffer = true })
-        map('n', '<Esc>', '<cmd>BufferClose<CR><cmd>wincmd c<CR>', { buffer = true })
+        vim.o.wrap = false
+
+        map('n', '<CR>',  '<Plug>RestNvim:w<CR>', { buffer = true })
+        map('n', '<Esc>', '<cmd>BufferClose<CR><cmd>wincmd c<CR>', {
+          buffer = true,
+        })
       end,
       group = 'RestNvim'
     })
