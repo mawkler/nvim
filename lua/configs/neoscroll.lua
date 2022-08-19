6feaaff6d9688572a2bdb40824d9744ce009fc28
Melker Österberg <melkerosterberg@gmail.com>
2022-08-19 21:51:04 +0200

    Don't load neoscroll.nvim in Neovide

--- lua/configs/neoscroll.lua
+++ lua/configs/neoscroll.lua
@@ -3,10 +3,11 @@
 ---------------
 return { 'karb94/neoscroll.nvim',
   event = 'WinScrolled',
+  cond = function() return not vim.g.neovide end,
   config = function()
     local scroll_speed = 140
-    require('neoscroll').setup { easing_function = 'cubic' }
-    require('neoscroll.config').set_mappings {
+    require('neoscroll').setup({ easing_function = 'cubic' })
+    require('neoscroll.config').set_mappings({
       ['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', scroll_speed}},
       ['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', scroll_speed}},
       ['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', scroll_speed}},
@@ -14,6 +15,6 @@ return { 'karb94/neoscroll.nvim',
       ['zt']    = {'zt', {scroll_speed}, 'sine'},
       ['zz']    = {'zz', {scroll_speed}, 'sine'},
       ['zb']    = {'zb', {scroll_speed}, 'sine'},
-    }
+    })
   end
 }
