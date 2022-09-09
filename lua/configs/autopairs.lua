a4a943fb87e0dd92e24794fed532382d9ff87127
Melker Österberg <melkerosterberg@gmail.com>
2022-09-09 15:24:44 +0200

    Disable nvim-autopairs in DressingInput

--- lua/configs/autopairs.lua
+++ lua/configs/autopairs.lua
@@ -4,17 +4,20 @@
 return { 'windwp/nvim-autopairs',                -- Auto-close brackets, etc.
   event = 'InsertEnter' ,
   config = function()
+    local rule = require('nvim-autopairs.rule')
+    local autopairs = require('nvim-autopairs')
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
     -- Auto insert `()` after completing a function or method
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done {
       map_char = { tex = '' },
     })
 
-    local rule = require('nvim-autopairs.rule')
-    local autopairs = require('nvim-autopairs')
+    autopairs.setup({
+      map_c_h = true,
+      disable_filetype = { 'TelescopePrompt', 'DressingInput' },
+    })
 
-    autopairs.setup { map_c_h = true }
     autopairs.add_rules {
       rule('$', '$', 'tex'),
       rule('*', '*', 'markdown'),
