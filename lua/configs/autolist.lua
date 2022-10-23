218255b045c85c2676da2b2bae8509303fb70bbe
Melker Österberg <melkerosterberg@gmail.com>
2022-10-23 15:24:02 +0200

    Add <leader>x mapping for toggling checkboxes

--- lua/configs/autolist.lua
+++ lua/configs/autolist.lua
@@ -4,19 +4,11 @@
 return { 'gaoDean/autolist.nvim',
   ft = { 'markdown', 'text', 'latex' },
   config = function()
-    require('autolist').setup({
-    	invert = {
-    		normal_mapping = '<space>x',
-    	},
-    	checkbox = {
-    		left = '%[',
-    		right = '%] ',
-    		fill = 'x',
-    	},
-    	colon = {
-    		indent_raw = false,
-    		indent = false,
-    	},
-    })
-  end,
+    local map = require('utils').map
+    local autolist = require('autolist')
+
+    autolist.setup()
+
+    map('n', '<leader>x', autolist.invert)
+  end
 }
