4daf2f3f5c304a3cfa40473b968c72aba9b41ede
Melker Österberg <melkerosterberg@gmail.com>
2022-11-03 08:28:51 +0100

    Add substitute.nvim linewise command succ

--- lua/configs/substitute.lua
+++ lua/configs/substitute.lua
@@ -17,24 +17,27 @@ return { 'gbprod/substitute.nvim',
     local substitute = require('substitute')
     local exchange = require('substitute.exchange')
 
-    local function sub_clipboard_op()
-      substitute.operator({ register = '+' })
+    local function sub_clipboard(action)
+      return function()
+        substitute[action]({ register = '+' })
+      end
     end
 
     local function sub_clipboard_visual()
       substitute.visual({ register = '+' })
     end
 
-    map('n', 'su',  substitute.operator,  'Substitute operator')
-    map('n', 'suu', substitute.line,      'Substitute line')
-    map('n', 'sU',  substitute.eol,       'Substitute eol')
-    map('x', 'su',  substitute.visual,    'Substitute visual')
-    map('n', 'suc', sub_clipboard_op,     'Substitute (clipboard)')
-    map('x', 'sU',  sub_clipboard_visual, 'Substitute (clipboard)')
-    map('n', 'sx',  exchange.operator,    'Exchange operator')
-    map('n', 'sxx', exchange.line,        'Exchange line')
-    map('x', 'X',   exchange.visual,      'Exchange visual')
-    map('n', 'sxc', exchange.cancel,      'Exchange cancel')
+    map('n', 'su',   substitute.operator,       'Substitute operator')
+    map('n', 'suu',  substitute.line,           'Substitute line')
+    map('n', 'sU',   substitute.eol,            'Substitute eol')
+    map('x', 'su',   substitute.visual,         'Substitute visual')
+    map('n', 'suc',  sub_clipboard('operator'), 'Substitute (clipboard)')
+    map('n', 'succ', sub_clipboard('line'),     'Substitute (clipboard)')
+    map('x', 'sU',   sub_clipboard_visual,      'Substitute (clipboard)')
+    map('n', 'sx',   exchange.operator,         'Exchange operator')
+    map('n', 'sxx',  exchange.line,             'Exchange line')
+    map('x', 'X',    exchange.visual,           'Exchange visual')
+    map('n', 'sxc',  exchange.cancel,           'Exchange cancel')
 
     -- Make sure Yanky is loaded
     if not packer_plugins['yanky.nvim'].loaded then
