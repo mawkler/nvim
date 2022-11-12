0e127c822e14bf8868cf1414d421351283fa1504
Melker Österberg <melkerosterberg@gmail.com>
2022-11-12 13:29:22 +0100

    Switch to yanky integration for substitute.nvim

--- lua/configs/substitute.lua
+++ lua/configs/substitute.lua
@@ -6,6 +6,7 @@ return { 'gbprod/substitute.nvim',
     { 'n', 'su' },
     { 'x', 'su' },
     { 'n', 'sU' },
+    { 'x', 'sU' },
     { 'x', '<leader>su' },
     { 'n', 'sx' },
     { 'x', 'X' },
@@ -36,7 +37,7 @@ return { 'gbprod/substitute.nvim',
     map('x', 'sU',   sub_clipboard_visual,      'Substitute (clipboard)')
     map('n', 'sx',   exchange.operator,         'Exchange operator')
     map('n', 'sxx',  exchange.line,             'Exchange line')
-    map('x', 'X',    exchange.visual,           'Exchange visual')
+    map('x', 'sx',   exchange.visual,           'Exchange visual')
     map('n', 'sxc',  exchange.cancel,           'Exchange cancel')
 
     -- Make sure Yanky is loaded
@@ -45,14 +46,7 @@ return { 'gbprod/substitute.nvim',
     end
 
     substitute.setup({
-      on_substitute = function(event)
-        require('yanky').init_ring(
-          'p',
-          event.register,
-          event.count,
-          event.vmode:match('[vV]')
-        )
-      end,
+      on_substitute = require('yanky.integration').substitute(),
     })
   end
 }
