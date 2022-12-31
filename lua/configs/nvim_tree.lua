7abf039464e44e0388d2bc82485af16bb7d0c3c6
Melker Ulander <melker.ulander@pm.me>
2022-12-31 14:35:46 +0100

    Conceal cursor in NvimTree buffer

--- lua/configs/nvim_tree.lua
+++ lua/configs/nvim_tree.lua
@@ -102,5 +102,27 @@ return { 'kyazdani42/nvim-tree.lua',
     vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', {
       link = 'IndentBlanklineChar',
     })
+
+    vim.api.nvim_create_autocmd({ 'WinEnter', 'BufWinEnter' }, {
+      pattern = 'NvimTree*',
+      callback = function()
+        local def = vim.api.nvim_get_hl_by_name('Cursor', true)
+        vim.api.nvim_set_hl(0, 'Cursor', vim.tbl_extend('force', def, {
+          blend = 100,
+        }))
+        vim.opt.guicursor:append('a:Cursor/lCursor')
+      end,
+    })
+
+    vim.api.nvim_create_autocmd({ 'BufLeave', 'WinClosed' }, {
+      pattern = 'NvimTree*',
+      callback = function()
+        local def = vim.api.nvim_get_hl_by_name('Cursor', true)
+        vim.api.nvim_set_hl(0, 'Cursor', vim.tbl_extend('force', def, {
+          blend = 0,
+        }))
+        vim.opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20'
+      end,
+    })
   end
 }
