0e2b0702fc0398dc4b55a31a6e0a06dc07d792d0
Melker Ulander <melker.ulander@pm.me>
2023-01-03 16:56:43 +0100

    Set default CSV separator to Tab

--- lua/configs/csv.lua
+++ lua/configs/csv.lua
@@ -4,10 +4,11 @@
 return { 'chrisbra/csv.vim',
   ft = 'csv',
   setup = function()
-    -- Disable some default mappings
-    vim.g.csv_nomap_W = 1
-    vim.g.csv_nomap_E = 1
-    vim.g.csv_nomap_L = 1
-    vim.g.csv_nomap_H = 1
+    vim.g.csv_default_delim = '\t'
+    local disabled_mappings = {'e', 'l', 'h'}
+
+    for _, map in pairs(disabled_mappings) do
+      vim.g['csv_nomap_' .. map] = 1
+    end
   end
 }
