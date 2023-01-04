02ef7e6d340779bac682e7e003f6446bead83205
Melker Ulander <melker.ulander@pm.me>
2023-01-04 10:29:32 +0100

    Remove redundant HTTP file type detection

--- lua/configs/rest.lua
+++ lua/configs/rest.lua
@@ -4,40 +4,31 @@
 return { 'NTBBloodbath/rest.nvim',
   cmd = 'Http',
   ft = 'http',
-  setup = function()
-    vim.api.nvim_create_augroup('RestNvim', {})
-    vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-      pattern = '*.http',
-      callback = function()
-        vim.o.filetype = 'http'
-      end,
-      group = 'RestNvim'
-    })
-  end,
   config = function()
     local map = require('utils').map
 
     require('rest-nvim').setup()
 
-    local function http_request()
-      if vim.api.nvim_win_get_width(vim.api.nvim_get_current_win()) < 80 then
-        vim.cmd.wincmd('s')
-      else
-        vim.cmd.wincmd('v')
-      end
-      vim.cmd.edit('~/.config/nvim/http')
-      vim.o.filetype = 'http'
-      vim.o.buftype = ''
+    vim.api.nvim_create_user_command(
+      'Http',
+      function()
+        if vim.api.nvim_win_get_width(vim.api.nvim_get_current_win()) < 80 then
+          vim.cmd.wincmd('s')
+        else
+          vim.cmd.wincmd('v')
+        end
+        vim.cmd.edit('~/.config/nvim/http')
+        vim.o.filetype = 'http'
+        vim.o.buftype = ''
 
-      map('n', '<Esc>', '<cmd>BufferClose<CR><cmd>wincmd c<CR>', {
-        buffer = true,
-      })
-    end
+        map('n', '<Esc>', '<cmd>BufferClose<CR><cmd>wincmd c<CR>', {
+          buffer = true,
+        })
+      end,
+      { desc = 'Send HTTP request' }
+    )
 
-    vim.api.nvim_create_user_command('Http', http_request, {
-      desc = 'Send HTTP request',
-    })
-
+    vim.api.nvim_create_augroup('RestNvim', {})
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'http',
       callback = function()
