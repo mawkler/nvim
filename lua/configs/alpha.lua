72fae7d9f48555dbc620e9d44c70e9121581fedd
Melker Ulander <melker.ulander@pm.me>
2023-02-01 16:43:21 +0100

    Adapt alpha.nvim config to lazy.nvim

--- lua/configs/alpha.lua
+++ lua/configs/alpha.lua
@@ -4,93 +4,94 @@
 return { 'goolord/alpha-nvim',
   dependencies = { 'kyazdani42/nvim-web-devicons' },
   event = 'VimEnter',
-  config = function ()
-    import('alpha', function(alpha)
-      local dashboard = require('alpha.themes.dashboard')
-      local section = dashboard.section
-      local fn = vim.fn
+  config = function()
+    local alpha = require('alpha')
+    local dashboard = require('alpha.themes.dashboard')
+    local lazy_plugins = require('lazy').plugins()
+    local section = dashboard.section
+    local fn = vim.fn
 
-      -- Header
-      section.header.opts.hl = 'AlphaHeader'
-      section.header.val = {
-        '                                                     ',
-        '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
-        '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
-        '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
-        '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
-        '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
-        '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
-        '                                                     ',
-      }
+    -- Header
+    section.header.opts.hl = 'AlphaHeader'
+    section.header.val = {
+      '                                                     ',
+      '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+      '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+      '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+      '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+      '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+      '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+      '                                                     ',
+    }
 
-      -- Menu
-      section.buttons.val = {
-        dashboard.button('<Leader>s', '  Sessions'),
-        dashboard.button('<Leader>m', '  Most recent files'),
-        dashboard.button('<C-p>',     '  Find file'),
-        dashboard.button('i',         '  New file',           ':enew <BAR> startinsert<CR>'),
-        dashboard.button('<C-q>',     '  Quit'),
-      }
+    -- Menu
+    section.buttons.val = {
+      dashboard.button('<Leader>s', '  Sessions'),
+      dashboard.button('<Leader>m', '  Most recent files'),
+      dashboard.button('<C-p>',     '  Find file'),
+      dashboard.button('i',         '  New file',           ':enew <BAR> startinsert<CR>'),
+      dashboard.button('<C-q>',     '  Quit'),
+    }
 
-      -- Footer
-      local Footer = { items = {} }
+    -- Footer
+    local Footer = { items = {} }
 
-      function Footer:add(icon, item, condition)
-        if condition == nil or condition then
-          table.insert(self.items, string.format('%s %s', icon, tostring(item)))
-        end
+    function Footer:add(icon, item, condition)
+      if condition == nil or condition then
+        table.insert(self.items, string.format('%s %s', icon, tostring(item)))
       end
+    end
 
-      function Footer:create()
-        return table.concat(self.items, '  |  ')
-      end
+    function Footer:create()
+      return table.concat(self.items, '  |  ')
+    end
 
-      local loaded_plugins_count = #vim.tbl_filter(function(plugin)
-        return plugin.loaded
-      end, packer_plugins)
-      local total_plugins_count = #vim.tbl_keys(packer_plugins)
-      local loaded_plugins = string.format(
-        '%d/%d plugins',
-        loaded_plugins_count,
-        total_plugins_count
-      )
+    local loaded_plugins_count = #vim.tbl_filter(function(plugin)
+      return plugin._.loaded -- TODO: adapt this to Lazy
+    end, lazy_plugins)
 
-      local version = vim.version()
-      local date = os.date('%d-%m-%Y')
-      local version_string = string.format(
-        'v%s.%d.%d',
-        version.major,
-        version.minor,
-        version.patch
-      )
-      local import_failure_count = require('import').get_failure_count()
+    local total_plugins_count = #vim.tbl_keys(lazy_plugins)
+    local loaded_plugins = string.format(
+      '%d/%d plugins',
+      loaded_plugins_count,
+      total_plugins_count
+    )
 
-      Footer:add(
-        '',
-        tostring(import_failure_count) .. ' import(s) failed',
-        import_failure_count > 0
-      )
-      Footer:add('', loaded_plugins)
-      Footer:add('', version_string)
-      Footer:add('', date)
+    local version = vim.version() or {}
+    local date = os.date('%d-%m-%Y')
+    local version_string = string.format(
+      'v%s.%d.%d',
+      version.major,
+      version.minor,
+      version.patch
+    )
+    local import_failure_count = require('import').get_failure_count()
 
-      section.footer.val = Footer:create()
-      section.footer.opts.hl = 'NonText'
+    Footer:add(
+      '',
+      tostring(import_failure_count) .. ' import(s) failed',
+      import_failure_count > 0
+    )
+    Footer:add('', loaded_plugins)
+    Footer:add('', version_string)
+    Footer:add('', date)
 
-      -- Layout
-      local topMarginRatio = 0.2
-      local headerPadding = fn.max({ 2, fn.floor(fn.winheight(0) * topMarginRatio) })
+    section.footer.val = Footer:create()
+    section.footer.opts.hl = 'NonText'
 
-      dashboard.config.layout = {
-        { type = 'padding', val = headerPadding },
-        section.header,
-        { type = 'padding', val = 2 },
-        section.buttons,
-        { type = 'padding', val = 2 },
-        section.footer,
-      }
+    -- Layout
+    local topMarginRatio = 0.2
+    local headerPadding = fn.max({ 2, fn.floor(fn.winheight(0) * topMarginRatio) })
 
-      alpha.setup(dashboard.opts)
-    end)
+    dashboard.config.layout = {
+      { type = 'padding', val = headerPadding },
+      section.header,
+      { type = 'padding', val = 2 },
+      section.buttons,
+      { type = 'padding', val = 2 },
+      section.footer,
+    }
+
+    alpha.setup(dashboard.opts)
   end
 }
