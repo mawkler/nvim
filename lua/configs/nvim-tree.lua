ec503844355ae06f972b5fbecc2caded31852acf
Melker Ulander <melker.ulander@pm.me>
2023-02-27 16:23:39 +0100

    Update nvim-tree config to new on_attach behaviour

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -11,12 +11,10 @@ return { 'kyazdani42/nvim-tree.lua',
     'NvimTreeFindFile',
   },
   config = function()
-    local map = require('utils').map
-
     local nvim_tree, api = require('nvim-tree'), require('nvim-tree.api')
-    local callback = require('nvim-tree.config').nvim_tree_callback
+    local node, tree, fs, marks = api.node, api.tree, api.fs, api.marks
 
-    nvim_tree.setup {
+    nvim_tree.setup({
       diagnostics = {
         enable = true,
         show_on_dirs = true
@@ -55,27 +53,65 @@ return { 'kyazdani42/nvim-tree.lua',
       },
       view = {
         width = 40,
-        mappings = {
-          list = {
-            { key = 'l',     cb = callback('edit') },
-            { key = 'h',     cb = callback('close_node') },
-            { key = 'C',     cb = callback('collapse_all') },
-            { key = '>',     cb = callback('cd') },
-            { key = '<',     cb = callback('dir_up') },
-            { key = 'd',     cb = callback('trash') },
-            { key = 'D',     cb = callback('remove') },
-            { key = '<C-r>', cb = callback('refresh') },
-            { key = 'R',     cb = callback('full_rename') },
-            { key = '<Tab>', cb = callback('preview') },
-            { key = '<C-s>', cb = callback('split') },
-            { key = 'gh',    cb = callback('show_file_info') },
-            { key = ']g',    cb = callback('next_git_item') },
-            { key = '[g',    cb = callback('prev_git_item') },
-            { key = '<C-e>', cb = '' },
-          }
-        }
-      }
-    }
+      },
+      on_attach = function(bufnr)
+        local map = function(lhs, rhs, desc)
+          require('utils').map('n', lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        -- Custom mappings
+        map('l',     node.open.edit,             'Open')
+        map('h',     node.navigate.parent_close, 'Close')
+        map(']g',    node.navigate.git.next,     'Next git node')
+        map('[g',    node.navigate.git.prev,     'Previous git node')
+        map('<Tab>', node.open.preview,          'Preview')
+        map('<C-s>', node.open.horizontal,       'Open in split')
+        map('gh',    node.show_info_popup,       'File information')
+        map('C',     tree.collapse_all,          'Close all')
+        map('>',     tree.change_root_to_node,   'CD')
+        map('<',     tree.change_root_to_parent, 'CD up')
+        map('<C-r>', tree.reload,                'Refresh')
+        map('<Esc>', tree.close,                 'Close')
+        map('d',     fs.trash,                   'Trash')
+        map('D',     fs.remove,                  'Delete')
+
+        -- Recommended defaults
+        map('<C-t>', node.open.tab,                  'Open: New Tab')
+        map('<C-v>', node.open.vertical,             'Open: Vertical Split')
+        map('>',     node.navigate.sibling.next,     'Next Sibling')
+        map('<',     node.navigate.sibling.prev,     'Previous Sibling')
+        map('.',     node.run.cmd,                   'Run Command')
+        map(']e',    node.navigate.diagnostics.next, 'Next Diagnostic')
+        map('[e',    node.navigate.diagnostics.prev, 'Prev Diagnostic')
+        map('J',     node.navigate.sibling.last,     'Last Sibling')
+        map('K',     node.navigate.sibling.first,    'First Sibling')
+        map('P',     node.navigate.parent,           'Parent Directory')
+        map('s',     node.run.system,                'Run System')
+        map('B',     tree.toggle_no_buffer_filter,   'Toggle No Buffer')
+        map('C',     tree.toggle_git_clean_filter,   'Toggle Git Clean')
+        map('H',     tree.toggle_hidden_filter,      'Toggle Dotfiles')
+        map('I',     tree.toggle_gitignore_filter,   'Toggle Git Ignore')
+        map('S',     tree.search_node,               'Search')
+        map('U',     tree.toggle_custom_filter,      'Toggle Hidden')
+        map('g?',    tree.toggle_help,               'Help')
+        map('W',     tree.collapse_all,              'Collapse')
+        map('F',     api.live_filter.clear,          'Clean Filter')
+        map('f',     api.live_filter.start,          'Filter')
+        map('m',     marks.toggle,                   'Toggle Bookmark')
+        map('bmv',   marks.bulk.move,                'Move Bookmarked')
+        map('a',     fs.create,                      'Create')
+        map('c',     fs.copy.node,                   'Copy')
+        map('gy',    fs.copy.absolute_path,          'Copy Absolute Path')
+        map('p',     fs.paste,                       'Paste')
+        map('r',     fs.rename,                      'Rename')
+        map('x',     fs.cut,                         'Cut')
+        map('y',     fs.copy.filename,               'Copy Name')
+        map('Y',     fs.copy.relative_path,          'Copy Relative Path')
+
+        map('<2-LeftMouse>',  api.node.open.edit,       'Open')
+        map('<2-RightMouse>', tree.change_root_to_node, 'CD')
+      end,
+    })
 
     vim.api.nvim_create_augroup('NvimTreeRefresh', {})
     vim.api.nvim_create_autocmd('BufEnter', {
@@ -95,6 +131,7 @@ return { 'kyazdani42/nvim-tree.lua',
     remove_highlight('NvimTreeFileNew')
     remove_highlight('NvimTreeFileDeleted')
 
+    local map = require('utils').map
     map('n', '<leader>`', api.tree.toggle, 'Toggle file tree')
     map('n', '<leader>~', vim.cmd.NvimTreeFindFile, 'Show current file in file tree')
 
