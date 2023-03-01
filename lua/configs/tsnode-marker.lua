0c4e0a86adc513b8236665c4bbce30eac3e44791
Melker Ulander <melker.ulander@pm.me>
2023-03-01 17:20:39 +0100

    Include Markdown fence delimiters in tsnode-marker

--- lua/configs/tsnode-marker.lua
+++ lua/configs/tsnode-marker.lua
@@ -10,8 +10,21 @@ return {
       pattern = 'markdown',
       callback = function(context)
         require('tsnode-marker').set_automark(context.buf, {
-          target = { 'code_fence_content' }, -- List of target node types
-          hl_group = 'CursorLine', -- Highlight group
+          target = { 'fenced_code_block' },
+          hl_group = 'CursorLine',
+          range = function(_, node)
+            -- Copied from README
+            local start_row, start_col, end_row, end_col = node:range()
+
+            for i = node:child_count() - 1, 2, -1 do
+              local n = node:child(i)
+              if n:type() == 'fenced_code_block_delimiter' then
+                _, _, end_row, end_col = n:range()
+              end
+            end
+
+            return start_row, start_col, end_row, end_col
+          end,
         })
       end
     })
