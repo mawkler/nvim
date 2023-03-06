b6993c68b0da369fe49ab71403ffb7ae32cc0254
Melker Ulander <melker.ulander@pm.me>
2023-03-06 18:09:27 +0100

    Add <leader>o mapping to open Overseer

--- lua/configs/overseer.lua
+++ lua/configs/overseer.lua
@@ -3,9 +3,14 @@
 --------------
 return { 'stevearc/overseer.nvim',
   config = function()
-    require('overseer').setup(({
+    local overseer = require('overseer')
+    local map = require('utils').map
+
+    overseer.setup(({
       -- Template modules to load
       templates = { 'builtin' },
     }))
+
+    map('n', '<leader>o', overseer.open, 'Overseer')
   end
 }
