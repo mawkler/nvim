57f0bfb6a012681c43a2e2dbd96f0d2a985f3b82
Melker Ulander <melker.ulander@pm.me>
2023-05-31 12:05:40 +0200

    Adapt scrollview to new configuration API

--- lua/configs/scrollview.lua
+++ lua/configs/scrollview.lua
@@ -10,19 +10,20 @@ return {
     scrollview.setup({
       current_only = true,
       winblend = 50,
+      signs_on_startup = {}, -- Disable scrollview diagnostics
     })
 
     local augroup = vim.api.nvim_create_augroup('Scrollview', {})
     vim.api.nvim_create_autocmd({ 'WinScrolled' }, {
       group = augroup,
-      callback = scrollview.scrollview_enable,
+      callback = function() scrollview.set_state(true) end,
     })
     vim.api.nvim_create_autocmd({ 'CursorHold' }, {
       group = augroup,
       callback = function()
         -- Scrollview throws an error in command-line window for some reason
         if vim.fn.win_gettype() ~= 'command' then
-          scrollview.scrollview_disable()
+          scrollview.set_state(false)
         end
       end,
     })
