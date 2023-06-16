85aee356fb64b2ba376466217a8d43487f7896cb
Melker Ulander <melker.ulander@pm.me>
2023-06-16 17:10:52 +0200

    Use fidget.nvim's legacy branch

--- lua/configs/fidget.lua
+++ lua/configs/fidget.lua
@@ -4,6 +4,7 @@
 return {
   'j-hui/fidget.nvim',
   event = 'LspAttach',
+  tag = 'legacy', -- TODO: remove once fidget has been rewritten
   config = function()
     require('fidget').setup {
       text = { spinner = 'dots', done = '' },
