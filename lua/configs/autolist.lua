29fcae790a8b5a55d684361e4bd7b0ac823d394c
Melker Ulander <melker.ulander@pm.me>
2023-06-30 09:11:28 +0200

    Adapt to new autolist version

--- lua/configs/autolist.lua
+++ lua/configs/autolist.lua
@@ -1,37 +1,35 @@
 --------------
 -- Autolist --
 --------------
+local filetypes = { 'markdown', 'text', 'latex' }
 return {
   'gaoDean/autolist.nvim',
-  ft = { 'markdown', 'text', 'latex' },
+  ft = filetypes,
   config = function()
+    local map = require('utils').map
     local autolist = require('autolist')
 
     autolist.setup({
       colon = { indent_raw = false },
     })
 
-    local function create_mapping_hook(mode, mapping, hook, desc, alias)
-      vim.keymap.set(
-        mode,
-        mapping,
-        function(motion)
-          local keys = hook(motion, alias or mapping)
-          if not keys then keys = '' end
-          return keys
-        end,
-        { expr = true, buffer = true, desc = desc }
-      )
+    local opts = { buffer = true }
+
+    local function set_keymaps()
+      map('i', '<CR>',      '<CR><cmd>AutolistNewBullet<CR>',      opts)
+      map('n', 'o',         'o<cmd>AutolistNewBullet<CR>',         opts)
+      map('n', 'O',         'O<cmd>AutolistNewBulletBefore<CR>',   opts)
+      map('n', '<leader>x', '<cmd>AutolistToggleCheckbox<CR><CR>', opts)
     end
 
-    create_mapping_hook('i', '<CR>',      autolist.new,               'Autolist new line')
-    create_mapping_hook('i', '<C-t>',     autolist.indent,            'Autolist indent')
-    create_mapping_hook('i', '<C-d>',     autolist.indent,            'Autolist dedent')
-    create_mapping_hook('n', 'dd',        autolist.force_recalculate, 'Autolist delete line')
-    create_mapping_hook('n', 'o',         autolist.new,               'Autolist new line')
-    create_mapping_hook('n', 'O',         autolist.new_before,        'Autolist new line above')
-    create_mapping_hook('n', '>>',        autolist.indent,            'Autolist indent')
-    create_mapping_hook('n', '<<',        autolist.indent,            'Autolist dedent')
-    create_mapping_hook('n', '<leader>x', autolist.invert_entry,      'Autolist invert', '')
+    local augroup = vim.api.nvim_create_augroup('Autolist', {})
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = filetypes,
+      group = augroup,
+      callback = set_keymaps,
+    })
+
+    -- Set the keymaps the firs time autolist.nvim gets lazy-loaded
+    set_keymaps()
   end
 }
