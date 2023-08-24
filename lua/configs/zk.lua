9e243aa99140137ad244e5cc761763af25e44cf0
Melker Ulander <melker.ulander@pm.me>
2023-08-24 18:11:49 +0200

    Don't throw error if <leader>zn gets cancelled

--- lua/configs/zk.lua
+++ lua/configs/zk.lua
@@ -1,17 +1,25 @@
 --------
 -- zk --
 --------
+
+local function new_note()
+  local ok, title = pcall(vim.fn.input, 'Note title: ');
+  if ok and title and title ~= '' then
+    require('zk').new({ title = title })
+  end
+end
+
 return {
   'mickael-menu/zk-nvim',
   requires = 'nvim-lua/plenary.nvim',
   keys = {
-    { '<leader>zn', '<Cmd>ZkNew { title = vim.fn.input("Title: ") }<CR>', mode = 'n', desc = 'New note' },
+    { '<leader>zn', new_note, desc = 'New note' },
     { '<leader>zn', '<Cmd>ZkNewFromContentSelection<CR>', mode = 'x', desc = 'New note from selection' },
-    { '<leader>zf', '<Cmd>ZkNotes { sort = { "modified" } }<CR>', mode = 'n', desc = 'Find notes' },
-    { '<leader>zt', '<Cmd>ZkTags<CR>', mode = 'n', desc = 'Note tags' },
-    { '<leader>zc', '<Cmd>ZkCd<CR>', mode = 'n', desc = 'Change to notes directory' },
+    { '<leader>zf', '<Cmd>ZkNotes { sort = { "modified" } }<CR>', desc = 'Find notes' },
+    { '<leader>zt', '<Cmd>ZkTags<CR>', desc = 'Note tags' },
+    { '<leader>zc', '<Cmd>ZkCd<CR>', desc = 'Change to notes directory' },
     { '<leader>z/', ":'<,'>ZkMatch<CR>", mode = 'x', desc = 'Find in notes' },
-    { '<leader>z/', '<Cmd>ZkNotes { sort = { "modified" }, match = { vim.fn.input("Search: ") } }<CR>', mode = 'n', desc = 'Find in notes' },
+    { '<leader>z/', '<Cmd>ZkNotes { sort = { "modified" }, match = { vim.fn.input("Search: ") } }<CR>', desc = 'Find in notes' },
   },
   config = function()
     require('zk').setup({
