b91a678b261946bbc007c50a029ededafd5e17d4
Melker Ulander <melker.ulander@pm.me>
2023-10-03 17:16:48 +0200

    Don't show indent-blankline indent on first level

--- lua/configs/indent-blankline/init.lua
+++ lua/configs/indent-blankline/init.lua
@@ -5,12 +5,19 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   event = 'VeryLazy',
   main = 'ibl',
-  opts = {
-    show_first_indent_level = false,
-    indent = { char = '▏' },
-    exclude = {
-      filetypes = require('configs.indent-blankline.config').disabled_filetypes,
-      buftypes = { 'fzf', 'help', 'terminal', 'nofile' },
-    },
-  }
+  config = function()
+    -- Don't show lines on the first indent level
+    local hooks, builtin = require ('ibl.hooks'), require('ibl.hooks').builtin
+    hooks.register(hooks.type.WHITESPACE, builtin.hide_first_space_indent_level)
+    hooks.register(hooks.type.WHITESPACE, builtin.hide_first_tab_indent_level)
+
+    require('ibl').setup({
+      indent = { char = '▏' },
+      scope = { enabled = false, },
+      exclude = {
+        filetypes = require('configs.indent-blankline.config').disabled_filetypes,
+        buftypes = { 'fzf', 'help', 'terminal', 'nofile' },
+      },
+    })
+  end
 }
