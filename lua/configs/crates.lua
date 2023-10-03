281dab2c8d2bd62fb40214f3ff7bdecee1ef4651
Melker Ulander <melker.ulander@pm.me>
2023-10-03 17:12:39 +0200

    Add crates.nvim key mappings

--- lua/configs/crates.lua
+++ lua/configs/crates.lua
@@ -3,19 +3,13 @@
 ------------
 return {
   'saecki/crates.nvim',
-  tag = 'v0.3.0',
   dependencies = 'nvim-lua/plenary.nvim',
-  event = { "BufRead Cargo.toml" },
+  event = { 'BufRead Cargo.toml' },
   config = function()
-    local cmp = require('cmp')
+    local crates, cmp = require('crates'),  require('cmp')
+    local map = require('utils').map
 
-    require('crates').setup({
-      popup = {
-        keys = {
-          open_url = { 'gx' },
-          toggle_feature = { '<space>lf' },
-        },
-      },
+    crates.setup({
       null_ls = {
         enabled = true,
         name = 'crates.nvim',
@@ -35,5 +29,20 @@ return {
       callback = enable_cmp_cargo_completion,
     })
     enable_cmp_cargo_completion()
+
+    -- Keymaps
+    local opts = { silent = true }
+    map('n', '<leader>lf', crates.show_features_popup,                'Show crate features')
+    map('n', '<leader>lv', crates.show_versions_popup,                'Show crate versions')
+    map('n', '<leader>ld', crates.show_dependencies_popup,            'Show crate dependencies')
+    map('n', '<leader>lu', crates.upgrade_crate,                      'Upgrade crate')
+    map('x', '<leader>lu', crates.upgrade_crates,                     'Upgrade crates')
+    map('n', '<leader>lU', crates.upgrade_all_crates,                 'Upgrade all crates')
+    map('n', '<leader>lt', crates.expand_plain_crate_to_inline_table, 'Expand crate to inline table')
+    map('n', '<leader>lT', crates.extract_crate_into_table,           'Extract crate into table')
+    map('n', '<leader>lh', crates.open_homepage,                      'Open crate homepage')
+    map('n', '<leader>lr', crates.open_repository,                    'Open crate repository')
+    map('n', '<leader>ld', crates.open_documentation,                 'Open crate documentation')
+    map('n', '<leader>lc', crates.open_crates_io,                     'Open crate on crates.io')
   end
 }
