fa73df29397cd7874e9b37505100b68ebee3254b
Melker Ulander <melker.ulander@pm.me>
2023-10-12 08:27:37 +0200

    Switch to VeryLazy for dressing.nvim
    Fixes issue where alpha-nvim doesn't get horizontally centered when
    launching Neovide

--- lua/configs/dressing.lua
+++ lua/configs/dressing.lua
@@ -3,7 +3,7 @@
 --------------
 return {
   'stevearc/dressing.nvim',
-  event = 'CursorMoved',
+  event = 'VeryLazy',
   config = function()
     local utils = require('utils')
     local map, feedkeys = utils.map, utils.feedkeys
