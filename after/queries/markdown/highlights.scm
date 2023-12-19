59ef1c38f913e181e72ea31b96573a1875ab7d94
Melker Ulander <melker.ulander@pm.me>
2023-12-19 10:57:11 +0100

    Conceal markdown checkbox *and* preceding dash

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,9 +1,9 @@
 ; extends
 
-(list
-  (list_item
-    (task_list_marker_checked) @text.todo.checked.markdown (#set! conceal "")))
+((task_list_marker_checked) @text.todo.checked
+  (#offset! @text.todo.checked 0 -2 0 0)
+  (#set! conceal ""))
 
-(list
-  (list_item
-    (task_list_marker_unchecked) @text.todo.unchecked.markdown (#set! conceal "")))
+((task_list_marker_unchecked) @text.todo.unchecked
+  (#offset! @text.todo.unchecked 0 -2 0 0)
+  (#set! conceal ""))
