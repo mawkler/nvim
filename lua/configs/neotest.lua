abb3254f70a78bca657e2d34f8c5439b7769829d
Melker Ulander <melker.ulander@pm.me>
2023-12-20 09:14:17 +0100

    Refactor neotest lazy loading

--- lua/configs/neotest.lua
+++ lua/configs/neotest.lua
@@ -1,29 +1,45 @@
 -------------
 -- Neotest --
 -------------
+
+local function neotest()
+  return require('neotest')
+end
+
+local function run_file() neotest().run.run(vim.fn.expand("%")) end
+local function open() neotest().output.open({ enter = true }) end
+local function watch_file() neotest().watch.toggle(vim.fn.expand("%")) end
+local function jump_to_failed(direction, opts)
+  return neotest().jump[direction](opts)
+end
+
 return {
   'nvim-neotest/neotest',
-  dependencies = { 'haydenmeade/neotest-jest', 'rouge8/neotest-rust', 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'haydenmeade/neotest-jest',
+    'rouge8/neotest-rust',
+    'nvim-lua/plenary.nvim',
+  },
   keys = {
-    '<leader>Tr',
-    '<leader>Tc',
-    '<leader>TR',
-    '<leader>To',
-    '<leader>Tw',
-    '<leader>TO',
-    '<leader>Tm',
-    '<leader>Ts',
-    ']t',
-    '[t',
-    ']T',
-    '[T',
+    { '<leader>Tr', function() neotest().run.run() end,             'Run test' },
+    { '<leader>Tc', function() neotest().run.stop() end,            'Cancel running test' },
+    { '<leader>TR', function() run_file() end,                      'Run tests in file' },
+    { '<leader>To', function() open() end,                          'Open test output' },
+    { '<leader>Tw', function() neotest().watch.toggle() end,        'Watch tests' },
+    { '<leader>TW', function() watch_file() end,                    'Watch tests in file' },
+    { '<leader>TO', function() neotest().output_panel.toggle() end, 'Open test output panel' },
+    { '<leader>Tm', function() neotest().summary.marked() end,      'Run marked tests' },
+    { '<leader>Ts', function() neotest().summary.toggle() end,      'Toggle teset summary' },
+    { ']t',         function() neotest().jump.next() end,           'Jump to next test' },
+    { '[t',         function() neotest().jump.prev() end,           'Jump to previous test' },
+    { ']T',         function() jump_to_failed('next') end,          'Jump to next failed test' },
+    { '[T',         function() jump_to_failed('prev') end,          'Jump to next failed test' },
   },
   cmd = { 'Neotest' },
   config = function()
-    local neotest, neotest_jest = require('neotest'), require('neotest-jest')
-    local map = require('utils').map
+    local neotest_jest = require('neotest-jest')
 
-    neotest.setup({
+    neotest().setup({
       diagnostic = {
         severity = vim.diagnostic.severity.INFO
       },
@@ -55,26 +71,5 @@ return {
         require("neotest-rust"),
       },
     })
-
-    local function run_file() neotest.run.run(vim.fn.expand("%")) end
-    local function open() neotest.output.open({ enter = true }) end
-    local function watch_file() neotest.watch.toggle(vim.fn.expand("%")) end
-    local function jump_failed(direction, opts)
-      return function() neotest.jump[direction](opts) end
-    end
-
-    map('n', '<leader>Tr', neotest.run.run,             'Run test')
-    map('n', '<leader>Tc', neotest.run.stop,            'Cancel running test')
-    map('n', '<leader>TR', run_file,                    'Run tests in file')
-    map('n', '<leader>To', open,                        'Open test output')
-    map('n', '<leader>Tw', neotest.watch.toggle,        'Watch tests')
-    map('n', '<leader>TW', watch_file,                  'Watch tests in file')
-    map('n', '<leader>TO', neotest.output_panel.toggle, 'Open test output panel')
-    map('n', '<leader>Tm', neotest.summary.marked,      'Run marked tests')
-    map('n', '<leader>Ts', neotest.summary.toggle,      'Toggle teset summary')
-    map('n', ']t',         neotest.jump.next,           'Jump to next test')
-    map('n', '[t',         neotest.jump.prev,           'Jump to previous test')
-    map('n', ']T',         jump_failed('next'),         'Jump to next failed test')
-    map('n', '[T',         jump_failed('prev'),         'Jump to next failed test')
   end
 }
