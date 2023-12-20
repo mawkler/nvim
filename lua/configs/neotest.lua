3de226aa860d20c23f18d30d5be0046e16979402
Melker Ulander <melker.ulander@pm.me>
2023-12-20 08:55:55 +0100

    Improve neotest key mappings and icons

--- lua/configs/neotest.lua
+++ lua/configs/neotest.lua
@@ -3,12 +3,14 @@
 -------------
 return {
   'nvim-neotest/neotest',
-  dependencies = { 'haydenmeade/neotest-jest' },
+  dependencies = { 'haydenmeade/neotest-jest', 'rouge8/neotest-rust', 'nvim-lua/plenary.nvim' },
   keys = {
-    '<leader>Tt',
+    '<leader>Tr',
     '<leader>Tc',
-    '<leader>TT',
+    '<leader>TR',
     '<leader>To',
+    '<leader>Tw',
+    '<leader>TO',
     '<leader>Tm',
     '<leader>Ts',
     ']t',
@@ -16,6 +18,7 @@ return {
     ']T',
     '[T',
   },
+  cmd = { 'Neotest' },
   config = function()
     local neotest, neotest_jest = require('neotest'), require('neotest-jest')
     local map = require('utils').map
@@ -25,6 +28,12 @@ return {
         severity = vim.diagnostic.severity.INFO
       },
       icons = {
+        unknown = '',
+        passed = '',
+        failed = '',
+        running = '',
+        skipped = '',
+        watching = '',
         running_animated = {
           '⠋', '⠙', '⠹', '⠸', '⠼',
           '⠴', '⠦', '⠧', '⠇', '⠏',
@@ -43,24 +52,29 @@ return {
         neotest_jest({
           jestCommand = 'npm test --',
         }),
+        require("neotest-rust"),
       },
     })
 
     local function run_file() neotest.run.run(vim.fn.expand("%")) end
     local function open() neotest.output.open({ enter = true }) end
+    local function watch_file() neotest.watch.toggle(vim.fn.expand("%")) end
     local function jump_failed(direction, opts)
       return function() neotest.jump[direction](opts) end
     end
 
-    map('n', '<leader>Tt', neotest.run.run,        'Run test')
-    map('n', '<leader>Tc', neotest.run.stop,       'Cancel running test')
-    map('n', '<leader>TT', run_file,               'Run tests in file')
-    map('n', '<leader>To', open,                   'Open test output')
-    map('n', '<leader>Tm', neotest.summary.marked, 'Run marked tests')
-    map('n', '<leader>Ts', neotest.summary.toggle, 'Toggle teset summary')
-    map('n', ']t',         neotest.jump.next,      'Jump to next test')
-    map('n', '[t',         neotest.jump.prev,      'Jump to previous test')
-    map('n', ']T',         jump_failed('next'),    'Jump to next failed test')
-    map('n', '[T',         jump_failed('prev'),    'Jump to next failed test')
+    map('n', '<leader>Tr', neotest.run.run,             'Run test')
+    map('n', '<leader>Tc', neotest.run.stop,            'Cancel running test')
+    map('n', '<leader>TR', run_file,                    'Run tests in file')
+    map('n', '<leader>To', open,                        'Open test output')
+    map('n', '<leader>Tw', neotest.watch.toggle,        'Watch tests')
+    map('n', '<leader>TW', watch_file,                  'Watch tests in file')
+    map('n', '<leader>TO', neotest.output_panel.toggle, 'Open test output panel')
+    map('n', '<leader>Tm', neotest.summary.marked,      'Run marked tests')
+    map('n', '<leader>Ts', neotest.summary.toggle,      'Toggle teset summary')
+    map('n', ']t',         neotest.jump.next,           'Jump to next test')
+    map('n', '[t',         neotest.jump.prev,           'Jump to previous test')
+    map('n', ']T',         jump_failed('next'),         'Jump to next failed test')
+    map('n', '[T',         jump_failed('prev'),         'Jump to next failed test')
   end
 }
