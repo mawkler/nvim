f4936d5e0c7a1dcf8c9ecf42d1059c9eddefff21
Melker Ulander <melker.ulander@pm.me>
2024-01-05 13:41:07 +0100

    Change function formatting in utils.lua

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,10 +1,10 @@
 local M = {}
 
-M.termcodes = function(str)
+function M.termcodes(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-M.map = function(modes, lhs, rhs, opts)
+function M.map(modes, lhs, rhs, opts)
   if type(opts) == 'string' then
     opts = { desc = opts }
   end
@@ -12,21 +12,21 @@ M.map = function(modes, lhs, rhs, opts)
   vim.keymap.set(modes, lhs, rhs, options)
 end
 
-M.feedkeys = function(keys, mode)
+function M.feedkeys(keys, mode)
   if mode == nil then mode = 'in' end
   return vim.api.nvim_feedkeys(M.termcodes(keys), mode, true)
 end
 
-M.feedkeys_count = function(keys, mode)
+function M.feedkeys_count(keys, mode)
   return M.feedkeys(vim.v.count1 .. keys, mode)
 end
 
-M.error = function(message)
+function M.error(message)
   vim.api.nvim_echo({{ message, 'Error' }}, false, {})
 end
 
 --- Returns a new table with `element` appended to `tbl`
-M.append = function(tbl, element)
+function M.append(tbl, element)
   local new_table = vim.deepcopy(tbl)
   table.insert(new_table, element)
   return new_table
@@ -34,7 +34,7 @@ end
 
 --- Gets the buffer number of every visible buffer
 --- @return integer[]
-M.visible_buffers = function()
+function M.visible_buffers()
   return vim.tbl_map(vim.api.nvim_win_get_buf, vim.api.nvim_list_wins())
 end
 
@@ -46,7 +46,7 @@ local function lsp_server_has_references()
 end
 
 --- Clear all highlighted LSP references in all windows
-M.clear_lsp_references = function()
+function M.clear_lsp_references()
   vim.cmd.nohlsearch()
   if lsp_server_has_references() then
     vim.lsp.buf.clear_references()
@@ -57,7 +57,7 @@ M.clear_lsp_references = function()
 end
 
 --- Close every floating window
-M.close_floating_windows = function()
+function M.close_floating_windows()
   for _, win in pairs(vim.api.nvim_list_wins()) do
     -- Sometimes the window doesn't exist anymore for some reason
     local success, win_config = pcall(vim.api.nvim_win_get_config, win)
@@ -68,13 +68,13 @@ M.close_floating_windows = function()
 end
 
 --- Get Mason package install path
- M.get_install_path = function(package)
+function M.get_install_path(package)
   return require('mason-registry').get_package(package):get_install_path()
 end
 
 
 --- Import plugin config from external module in `lua/configs/`
-M.use = function(module)
+function M.use(module)
   local ok, m = pcall(require, string.format('configs.%s', module))
   if ok then
     return m
@@ -84,12 +84,12 @@ M.use = function(module)
   end
 end
 
-M.noice_is_loaded = function()
+function M.noice_is_loaded()
   local success, _ = pcall(require, 'noice.config')
   return success and require('noice.config')._running
 end
 
-M.plugin_is_loaded = function(plugin)
+function M.plugin_is_loaded(plugin)
   -- Checking with `require` and `pcall` will cause Lazy to load the plugin
   local plugins = require('lazy.core.config').plugins
   return not not plugins[plugin] and plugins[plugin]._.loaded
