33268c49b7b58d4e76cbd705d3d1b80d18dcb1ee
Melker Ulander <melker.ulander@pm.me>
2024-01-09 16:46:42 +0100

    Adapt to Neovide v0.12.0

--- ginit.vim
+++
@@ -1,5 +0,0 @@
-GuiFont! FiraCode Nerd Font:h11
-GuiTabline 0
-GuiPopupmenu 0
-call GuiWindowMaximized(1)
-GuiRenderLigatures 1
--- lua/configs/neovide.lua
+++ lua/configs/neovide.lua
@@ -3,4 +3,7 @@ local g = vim.g
 if vim.fn.exists('g:neovide') == 1 then
   g.neovide_floating_blur_amount_x = 3.0
   g.neovide_floating_blur_amount_y = 3.0
+  g.neovide_cursor_trail_size = 0.7
+  g.neovide_scroll_animation_length = 0.06
+  g.neovide_cursor_animate_command_line = true
 end
