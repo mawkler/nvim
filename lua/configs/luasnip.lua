57a9ac6c9e802be3591b4d6c8dc53c694b6f5c91
Melker Ulander <melker.ulander@pm.me>
2024-01-09 19:43:41 +0100

    Replace friendly-snippets with luasnip-snippets

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -3,34 +3,41 @@
 -------------
 return {
   'L3MON4D3/LuaSnip',
-  dependencies = 'mawkler/friendly-snippets', -- Snippets collection
+  dependencies = 'mireq/luasnip-snippets', -- Collection of snippets
   event = 'InsertEnter',
   config = function()
     local fn = vim.fn
     local feedkeys, map = require('utils').feedkeys, require('utils').map
 
     local luasnip = require('luasnip')
+    local snippets_snip_utils = require('luasnip_snippets.common.snip_utils')
     local s, sn   = luasnip.snippet, luasnip.snippet_node
     local t, i, d = luasnip.text_node, luasnip.insert_node, luasnip.dynamic_node
 
-    luasnip.config.setup { history = true }
+    luasnip.config.setup({ history = true })
+    snippets_snip_utils.setup()
+
+    luasnip.setup({
+      load_ft_func = snippets_snip_utils.load_ft_func,
+      ft_func = snippets_snip_utils.ft_func,
+    })
+
     luasnip.filetype_extend('all', {'global'})
-    require('luasnip.loaders.from_vscode').lazy_load() -- loads default paths
     require('luasnip.loaders.from_vscode').load({
       paths = { '~/.config/nvim/snippets' },
     })
 
-    local function clipboad_oneline_node()
+    local function clipboard_oneline_node()
       local clipboard, _ = fn.getreg('+'):gsub('\n', ' ')
       return clipboard
     end
 
     local luasnip_clipboard = function()
-      return sn(nil, i(1, clipboad_oneline_node()))
+      return sn(nil, i(1, clipboard_oneline_node()))
     end
 
     local plugin_repo_snippet = function()
-      local repo, _ = clipboad_oneline_node():gsub('.*github.com/([^/]*/[^/]*).*', '%1', 1)
+      local repo, _ = clipboard_oneline_node():gsub('.*github.com/([^/]*/[^/]*).*', '%1', 1)
       return sn(nil, i(1, repo))
     end
 
@@ -60,33 +67,15 @@ return {
     })
     luasnip.add_snippets('lua', {
       s({
-        trig = 'use',
-        name = 'Add plugin',
-        dscr = 'Add packer.nvim plugin from the clipboard'
-      }, {
-          t "use '", d(1, plugin_repo_snippet), t "'",
-        }),
-      s({
         trig = 'plugin',
         name = 'Add plugin config',
-        dscr = 'Add packer.nvim plugin from the clipboard'
+        dscr = 'Add plugin URL from the clipboard'
       }, {
-          t {
-            "return {",
-            "\t'",
-          },
+          t { "return {", "\t'" },
           d(1, plugin_repo_snippet), t "',",
-          t {
-            '',
-            '\tconfig = function()',
-            '\t\t',
-          },
+          t { '', '\tconfig = function()', '\t\t' },
           i(2),
-          t {
-            '',
-            '\tend',
-            '}'
-          }
+          t { '', '\tend', '}' }
         })
     })
 
@@ -106,7 +95,6 @@ return {
       end
     end
 
-
     local function toggle_active_choice()
       if luasnip.choice_active() then
         luasnip.change_choice(1)
@@ -115,6 +103,6 @@ return {
 
     map({'i', 's'}, '<M-S-l>', right_or_snip_next,   '<Right> or next snippet')
     map({'i', 's'}, '<M-S-h>', left_or_snip_prev,    '<Left> or previous snippet')
-    map({'i', 's'}, '<M-;>',   toggle_active_choice, 'Toggle active snippet choice')
+    map({'i', 's'}, '<M-t>',   toggle_active_choice, 'Toggle active snippet choice')
   end
 }
