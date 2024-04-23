9457d9badc804b4456f3715d4c156fa39cbebc71
Melker Ulander <melker.ulander@pm.me>
2024-04-23 10:32:16 +0200

    Add some go.nvim mappings

--- lua/configs/go.lua
+++ lua/configs/go.lua
@@ -10,11 +10,25 @@ return {
   },
   ft = { 'go', 'gomod' },
   config = function()
+    local function on_attach()
+      local map = require('utils').local_map(0)
+
+      vim.o.shiftwidth = 0 -- Use tabstop's width
+
+      map('n', '<leader>li', '<cmd>GoImports<CR>',         'Fix imports')
+      map('n', '<leader>ld', '<cmd>GoInstallBinaries<CR>', 'Make sure all tools are updated')
+      map('n', '<leader>lr', '<cmd>GoGenReturn<CR>',       'Go generate return values for function')
+      map('n', 'go',         '<cmd>GoAlt<CR>',             'Go alternative file')
+      map('n', '<C-w>go',    '<cmd>GoAltV<CR>',            'Go alternative file in vertical split')
+
+      map({'n', 'x'}, '<leader>lj', '<cmd>GoJson2Struct<CR>', 'Go struct from json')
+    end
+
     require('go').setup({
       lsp_cfg = true,
       lsp_inlay_hints = { enable = false }, -- Handled in lsp-inlay-hints.lua
+      lsp_keymaps = false,
+      lsp_on_attach = on_attach
     })
-
-    vim.o.shiftwidth = 0 -- Use tabstop's width
   end
 }
