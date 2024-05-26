33a57d78716f9f4285a0aab3e34c753ee349caaf
Melker Ulander <melker.ulander@pm.me>
2024-05-26 18:27:09 +0200

    Add <leader>\ mapping

--- plugin/visual-append.lua
+++ plugin/visual-append.lua
@@ -3,18 +3,22 @@ local map, feedkeys = require('utils').map, require('utils').feedkeys
 local append_chars = { '.', ',', ';', '!', '?' }
 
 local function visual_append(char)
-  vim.cmd('normal! m0')
-  feedkeys(':normal! A' .. char .. '<CR>:<Esc>')
-  vim.cmd('normal! `0')
+  return function()
+    vim.cmd('normal! m0')
+    feedkeys(':normal! A' .. char .. '<CR>:<Esc>')
+    vim.cmd('normal! `0')
 
-  vim.o.operatorfunc = 'v:lua.visual_append'
+    vim.o.operatorfunc = 'v:lua.visual_append'
+  end
 end
 
 local function visual_append_map(char)
   local desc = 'Visual append ' .. char
-  map({'n', 'x'}, '<leader>' .. char, function() visual_append(char) end, desc)
+  map({'n', 'x'}, '<leader>' .. char, visual_append(char), desc)
 end
 
 for _, char in pairs(append_chars) do
   visual_append_map(char)
 end
+
+map('n', '<leader>\\', visual_append(' \\'))
