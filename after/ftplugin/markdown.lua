18e7e322765cbe06ecf7ea751e4b3d05f46dbfd8
Melker Ulander <melker.ulander@pm.me>
2024-08-12 10:20:18 +0200

    Don't lazy load markview.nvim

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,3 @@
+local map = require('utils').map
+
+map('n', '<leader>lm', '<cmd>Markview toggle<CR>', { buffer = true, desc = 'Toggle Markview' })
--- lua/configs/markview.lua
+++ lua/configs/markview.lua
@@ -1,6 +1,9 @@
+--------------
+-- Markview --
+--------------
 return {
   'OXY2DEV/markview.nvim',
-  ft = 'markdown',
+  lazy = false, -- Markview handles lazy loading
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons'
