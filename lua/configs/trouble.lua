890f14f77ee65eec9f4f539c6af492d39bb26f5f
Melker Ulander <melker.ulander@pm.me>
2024-08-15 15:16:24 +0200

    Further improve trouble.nvim config

--- lua/configs/trouble.lua
+++ lua/configs/trouble.lua
@@ -5,15 +5,16 @@ return {
   'folke/trouble.nvim',
   cmd = 'TroubleToggle',
   keys = {
-    { '<leader>E', '<cmd>Trouble diagnostics focus<CR>', mode = 'n' }
+    { '<leader>E', '<cmd>Trouble diagnostics toggle<CR>', mode = 'n' }
   },
   config = function()
     require('trouble').setup({
       auto_preview = false,
-      use_diagnostic_signs = true,
       auto_close = true,
+      warn_no_results = false,
+      focus = true,
       keys = {
-        ['<Esc>']   = 'close',
+        ['<esc>']   = 'close',
         ['<C-q>']   = 'close',
         ['<C-c>']   = 'close',
         ['R']       = 'refresh',
@@ -25,6 +26,9 @@ return {
         ['[']       = 'prev',
         ['[[']      = false,
         [']]']      = false,
+      },
+      icons = {
+        kinds = require('utils.icons').icons,
       },
     })
   end
