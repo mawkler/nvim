a61ccb308053bf8eaff8b73bd68fc76837c8bf3e
Melker Ulander <melker.ulander@pm.me>
2024-08-28 15:31:28 +0200

    Use `dbg!` for printing with printer.nvim in Rust

--- lua/configs/printer.lua
+++ lua/configs/printer.lua
@@ -2,21 +2,31 @@
 -- Printer --
 -------------
 
-local function lua_formatter(inside, variable)
-  return string.format("print('%s: ' .. vim.inspect(%s))", inside, variable)
-end
-
 return {
   'rareitems/printer.nvim',
   keys = 'gp',
   config = function()
+    local function lua_formatter(inside, variable)
+      return string.format("print('%s: ' .. vim.inspect(%s))", inside, variable)
+    end
+
+    local function rust_formatter(inside, _)
+      local function starts_with(str, substr)
+        return string.sub(str, 1, 1) == substr
+      end
+
+      local prefix = starts_with(inside, '&') and '' or '&'
+      return string.format('dbg!(%s%s);', prefix, inside)
+    end
+
     require('printer').setup({
       keymap = 'gp',
       formatters = {
         lua = lua_formatter,
+        rust = rust_formatter,
       },
       add_to_inside = function(text)
-        return string.format("%s", text)
+        return string.format('%s', text)
       end,
     })
   end
