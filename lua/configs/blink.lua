9d6b85cfb5bf398f8897875cd2d90d9b6e2c6c54
Melker Ulander <melker.ulander@pm.me>
2025-03-14 14:24:05 +0100

    Increase blink.cmp update delay

--- lua/configs/blink.lua
+++ lua/configs/blink.lua
@@ -93,7 +93,7 @@ return {
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 0,
-          update_delay_ms = 0,
+          update_delay_ms = 50,
         },
       },
     })
