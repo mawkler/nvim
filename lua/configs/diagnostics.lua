79eb1cdafc7353a4d3220706b4945dac7385460c
Melker Ulander <melker.ulander@pm.me>
2026-04-04 23:09:09 +0200

    Remove deprecated vim.lsp.with
    I'm not sure what the config did, because removing it doesn't seem to
    make a different

--- lua/configs/diagnostics.lua
+++ lua/configs/diagnostics.lua
@@ -4,12 +4,6 @@
 
 local M = {}
 
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-  }
-)
-
 ---@param severity vim.diagnostic.Severity
 ---@return string
 function M.get_icon(severity)
