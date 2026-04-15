f0807742b42f678e4c55df6bf48cb78ebb43ada9
Melker Ulander <melker.ulander@pm.me>
2026-04-15 22:18:52 +0200

    Also use tiny-cmdline when searching

--- lua/configs/tiny-cmdline.lua
+++ lua/configs/tiny-cmdline.lua
@@ -1,15 +1,16 @@
 ------------------
 -- Tiny Cmdline --
 ------------------
+
+-- Assumes that ui2 has been enabled
 return {
   'rachartier/tiny-cmdline.nvim',
   init = function()
-    require('vim._core.ui2').enable({})
-
     vim.o.cmdheight = 0
 
     vim.g.tiny_cmdline = {
       on_reposition = require('tiny-cmdline').adapters.blink,
+      native_types = {}, -- Also use this plugin when searching
     }
   end,
 }
